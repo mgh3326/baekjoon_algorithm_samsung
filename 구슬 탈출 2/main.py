dfcf2866ffd5646e338e9bb3f530eabd9e12453d
kwanghyunMoon <mgh3326@naver.com>
2019-09-13 12:34:36 +0900

    구슬 탈출 complete

--- 구슬 탈출 2/main.py
+++ 구슬 탈출 2/main.py
@@ -1,7 +1,6 @@
 import sys
-import copy
 
-sys.stdin = open("./data/input1.txt", "r")
+sys.stdin = open("./data/my_input1.txt", "r")
 # a = open("./data/output1.txt", "r").read()
 # print(a)
 
@@ -9,66 +8,107 @@ dh = [-1, 0, 1, 0]  # 상 좌 하 우
 dw = [0, -1, 0, 1]  # 상 자 하 우
 
 
-def dfs(input_depth, input_dir_idx):
-    global result
-    is_end = False
+def run_red(current_red_point, nh, nw, current_blue_point):
+    while True:
+        next_red_point = current_red_point[0] + nh, current_red_point[1] + nw
+        if my_list[next_red_point[0]][next_red_point[1]] == -1:
+            break
+        elif next_red_point == tuple(current_blue_point):
+            break
+        elif my_list[next_red_point[0]][next_red_point[1]] == 1:
+            return 0
+        elif my_list[next_red_point[0]][next_red_point[1]] == 0:
+            current_red_point = list(next_red_point)
+    return current_red_point
 
-    if input_depth >= depth_max or (result != -1 and input_depth > result):
-        return
-    if input_depth != -1:
-        blue_point = point_list[0]
-        red_point = point_list[1]
-        nh, nw = dh[input_dir_idx], dw[input_dir_idx]
-        next_blue_point = blue_point[0] + nh, blue_point[1] + nw
-        next_red_point = red_point[0] + nh, red_point[1] + nw
-        next_blue_value = my_list[next_blue_point[0]][next_blue_point[1]]
-        next_red_value = my_list[next_red_point[0]][next_red_point[1]]
-        if next_blue_value == next_red_value == 1:  # 벽에 막혔을 경우
-            return
-        if next_blue_value == 0:
-            pass
-        elif next_blue_value == 1:
-            pass
-        elif next_blue_value == 3:
-            pass
-        elif next_blue_value == 4:
-            if result == -1 or result < input_depth + 1:
-                result = input_depth + 1
 
-        print()
-
-    next_depth = input_depth + 1
-    for dir_idx in range(len(dh)):
-        if dir_idx == input_dir_idx:
-            continue
-        saved_point_list.append(copy.deepcopy(point_list))
-        dfs(next_depth, dir_idx)
-        saved_point = saved_point_list.pop()
+def run_blue(current_blue_point, nh, nw, current_red_point):
+    while True:
+        next_blue_point = current_blue_point[0] + nh, current_blue_point[1] + nw
+        if my_list[next_blue_point[0]][next_blue_point[1]] == -1:
+            break
+        elif my_list[next_blue_point[0]][next_blue_point[1]] == 1:
+            return -1
+        elif next_blue_point == tuple(current_red_point):
+            break
+        elif my_list[next_blue_point[0]][next_blue_point[1]] == 0:
+            current_blue_point = list(next_blue_point)
+        else:
+            pass
+    return current_blue_point
 
 
 n, m = map(int, input().split())
 my_list = []
 depth_max = 10
-point_list = [[0, 0], [0, 0]]  # 파랑, 빨강
+point_list = [
+    [0, 0], [0, 0]
+]
+
 result = -1
 for i in range(n):
     temp_str = input()
     temp_list = []
     for _str in temp_str:
-        if _str == "#":
-            temp_list.append(1)
-        elif _str == "B":
-            temp_list.append(2)
-            point_list[0] = [i, temp_str.index(_str)]
-        elif _str == "R":
-            temp_list.append(3)
-            point_list[1] = [i, temp_str.index(_str)]
-
-        elif _str == ".":
+        if _str == "#":  # 벽
+            temp_list.append(-1)
+        elif _str == "B":  # Blue
             temp_list.append(0)
-        elif _str == "O":
-            temp_list.append(4)
+            point_list[1] = [i, temp_str.index(_str)]
+        elif _str == "R":
+            temp_list.append(0)  # Red
+            point_list[0] = [i, temp_str.index(_str)]
+        elif _str == ".":  # 움직일수 있는 공간
+            temp_list.append(0)
+        elif _str == "O":  # 블랙홀
+            temp_list.append(1)
     my_list.append(temp_list)
-saved_point_list = []
-dfs(-1, -1)
-print(result + 1)
+saved_point_set = set()
+saved_point_set.add((tuple(point_list[0]), tuple(point_list[1])))  # Red, Blue
+queue = [[point_list[0], point_list[1], 0]]
+
+while True:
+    if result != -1:
+        break
+    if len(queue) == 0:
+        break
+    start_red_point, start_blue_point, depth = queue.pop(0)
+    if depth == 10:
+        break
+    for dir_idx in range(len(dh)):
+        nh = dh[dir_idx]
+        nw = dw[dir_idx]
+        current_red_point = start_red_point.copy()
+        current_blue_point = start_blue_point.copy()
+        temp_point = current_red_point
+        if dir_idx == 0:  # 위일 경우
+            temp_point = min(current_red_point, current_blue_point, key=lambda x: x[0])
+        if dir_idx == 1:  # 좌일 경우
+            temp_point = min(current_red_point, current_blue_point, key=lambda x: x[1])
+        if dir_idx == 2:  # 하일 경우
+            temp_point = max(current_red_point, current_blue_point, key=lambda x: x[0])
+        if dir_idx == 3:  # 우일 경우
+            temp_point = max(current_red_point, current_blue_point, key=lambda x: x[1])
+        if temp_point == current_red_point:  # 레드 먼저
+            current_red_point = run_red(current_red_point, nh, nw, current_blue_point)
+            if current_red_point == 0:
+                current_blue_point = run_blue(current_blue_point, nh, nw, [0, 0])
+                if current_blue_point != -1:
+                    result = depth + 1
+                    break
+            else:
+                current_blue_point = run_blue(current_blue_point, nh, nw, current_red_point)
+        else:  # 블루 먼저
+            current_blue_point = run_blue(current_blue_point, nh, nw, current_red_point)
+            if current_blue_point == -1:
+                continue
+            current_red_point = run_red(current_red_point, nh, nw, current_blue_point)
+            if current_red_point == 0:
+                result = depth + 1
+                break
+        if current_blue_point == -1:
+            continue
+        if (tuple(current_red_point), tuple(current_blue_point)) not in saved_point_set:
+            saved_point_set.add((tuple(current_red_point), tuple(current_blue_point)))
+            queue.append([current_red_point, current_blue_point, depth + 1])
+print(result)
