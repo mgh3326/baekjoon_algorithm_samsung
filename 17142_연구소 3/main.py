0d66f0e6ed6c2b3a8cc82249c4747bedddfd4522
kwanghyunMoon <mgh3326@naver.com>
2019-10-14 15:56:10 +0900

    complete

--- 17142_연구소 3/main.py
+++ 17142_연구소 3/main.py
@@ -14,64 +14,49 @@ def dfs(current_idx, depth):
     global current_value
     global result
     if depth == m:
-        if current_value != 0:
-            return
-        if len(min_space_dict) == 0:
-            result = 0
-            return
-        if result == -1 or result > max(min_space_dict.values()):
-            result = max(min_space_dict.values())
-        return
-    for i in range(virus_len)[current_idx:]:
-        start_h, start_w = virus_list[i]
-        queue = [[start_h, start_w, 1]]
-        visit_dict = dict()
-        visit_dict[(start_h, start_w)] = 0
+        temp_set = set()
+        queue = []
+        temp_count = 0
+        max_cost = 0
+        for virus_idx in virus_set:
+            queue.append([virus_list[virus_idx], 0, False])
+            temp_set.add(tuple(virus_list[virus_idx]))
         while True:
-            # BFS로 채워가면 되겠다
             if len(queue) == 0:
                 break
-            current_h, current_w, cost = queue.pop(0)
-            for dh, dw in dir_list:
-                next_h = dh + current_h
-                next_w = dw + current_w
-                if next_h < 0 or next_w < 0 or next_h >= n or next_w >= n:
+            [current_h, current_w], cost, is_blank = queue.pop(0)
+            if result != -1 and cost > result:
+                return  # 백트래킹
+            if is_blank and cost > max_cost:
+                max_cost = cost
+            for dir_h, dir_w in dir_list:
+                nh = current_h + dir_h
+                nw = current_w + dir_w
+                if (nh >= n or nw >= n or nh < 0 or nw < 0) or (nh, nw) in temp_set:
                     continue
-                if board_list[next_h][next_w] == 0:
-                    if (next_h, next_w) in visit_dict:
-                        continue
-                    if (next_h, next_w) not in visit_dict:
-                        visit_dict[(next_h, next_w)] = 0
-                    visit_dict[(next_h, next_w)] += 1
-                    queue.append([next_h, next_w, cost + 1])
-                    if len(space_dict[(next_h, next_w)]) == 0:
-                        current_value -= 1
-                    if min_space_dict[(next_h, next_w)] == -1 or min_space_dict[(next_h, next_w)] > cost:
-                        min_space_dict[(next_h, next_w)] = cost
-                    space_dict[(next_h, next_w)].append(cost)
-        my_list.append(i)
+                if board_list[nh][nw] == 0:
+                    temp_set.add((nh, nw))
+                    queue.append([[nh, nw], cost + 1, True])
+                    temp_count += 1
+                if board_list[nh][nw] == 2:
+                    temp_set.add((nh, nw))
+                    queue.append([[nh, nw], cost + 1, False])
+        if temp_count == space_len:
+            if result == -1 or result > max_cost:
+                result = max_cost
+        return
+
+    for i in range(virus_len)[current_idx:]:
+        if i + 1 in virus_set:
+            continue
+        virus_set.add(i)
         dfs(i + 1, depth + 1)
-        my_list.pop()
-        for current_h, current_w in (visit_dict.keys()):
-            if current_h == start_h and current_h == start_h:
-                continue
-            value = visit_dict[current_h, current_w]
-            for i in range(value):
-                space_dict[(current_h, current_w)].pop()
-            if len(space_dict[(current_h, current_w)]) == 0:
-                min_space_dict[(current_h, current_w)] = -1
-                current_value += 1
-            else:
-                min_space_dict[(current_h, current_w)] = min(space_dict[(current_h, current_w)])
+        virus_set.remove(i)
 
 
 n, m = map(int, input().split())
 virus_list = []
-virus_dict = {}
 board_list = []
-space_list = []
-space_dict = {}
-min_space_dict = {}
 space_len = 0
 virus_len = 0
 result = -1
@@ -80,16 +65,13 @@ for i in range(n):
     for j in range(n):
         if temp_list[j] == 2:
             virus_list.append([i, j])
-            virus_dict[i, j] = 0
             virus_len += 1
         elif temp_list[j] == 0:
-            space_list.append([i, j])
-            space_dict[i, j] = []
-            min_space_dict[i, j] = -1
             space_len += 1
 
     board_list.append(temp_list)
 current_value = space_len
 my_list = []
+virus_set = set()
 dfs(0, 0)
 print(result)
